b7d8fd5e2137f5d81962f29aabcf9722cf284e34
Claudio Yanes <me@claudio4.com>
2023-10-26 01:08:10 +0100

    Autosave rust files

--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -1,19 +1,25 @@
-local mason_registry = require("mason-registry")
+local mason_registry = require "mason-registry"
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local codelldb = mason_registry.get_package("codelldb")
+local codelldb = mason_registry.get_package "codelldb"
 local extension_path = codelldb:get_install_path() .. "/extension/"
 local codelldb_path = extension_path .. "adapter/codelldb"
 local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+
+-- Save on edit for rust files
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  pattern = "*.rs",
+  command = "w",
+  nested = true,
+})
 
 return {
   dap = {
     adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
   },
   server = {
-  on_attach = on_attach,
-  capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
   },
 }
-
