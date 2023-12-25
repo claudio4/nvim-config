9ba6543eb96f486b4bfb84c0a017f52507961b25
Claudio Yanes <me@claudio4.com>
2023-12-25 04:13:43 +0000

    Fix suggested actions in welcome screen

--- lua/plugins/welcome.lua
+++ lua/plugins/welcome.lua
@@ -13,6 +13,15 @@ return {
         [[╚██████╗███████╗██║  ██║╚██████╔╝██████╔╝██║╚██████╔╝     ██║]],
         [[ ╚═════╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═╝ ╚═════╝      ╚═╝]],
       }
+
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+        dashboard.button("SPC e", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+        dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+        dashboard.button("SPC fg", "  > Live grep", "<cmd>Telescope live_grep<CR>"),
+        dashboard.button("SPC fo", "󰁯  > Explore Previous files", "<cmd>Telescope oldfiles<CR>"),
+      }
+
       require('alpha').setup(dashboard.config)
     end
   }
