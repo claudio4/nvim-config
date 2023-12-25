5c05eede35ce5301998b020f973938f076dc6614
Claudio Yanes <me@claudio4.com>
2023-12-25 04:14:11 +0000

    Add completations

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,7 +1,63 @@
 return {
   {
+    "hrsh7th/nvim-cmp",
+    event = { "VeryLazy", "InsertEnter" },
+    dependencies = {
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-path", -- source for file system paths
+      { "L3MON4D3/LuaSnip", version = 'v2.*', build = "make install_jsregexp" }, -- snippet engine
+      "saadparwaiz1/cmp_luasnip", -- for autocompletion
+      "rafamadriz/friendly-snippets", -- useful snippets
+    },
+    config = function ()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        snippet = { -- configure how nvim-cmp interacts with snippet engine
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+          ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+          ["<Esc>"] = cmp.mapping.abort(), -- close completion window
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        }),
+        sources = cmp.config.sources({
+          -- { name = "nvim_lsp" }, -- LSP
+          { name = "luasnip" }, -- snippets
+          { name = "buffer" }, -- text within current buffer
+          { name = "path" }, -- file system paths
+        }),
+      })
+    end
+  },
+  {
     'windwp/nvim-autopairs',
     event = { "VeryLazy", "InsertEnter" },
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      local autopairs = require("nvim-autopairs")
+
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+      })
+
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
   }
 }
 
