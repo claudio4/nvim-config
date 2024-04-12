d74755a7208476a777635582096b6fd4b89f1892
Claudio Yanes <me@claudio4.com>
2024-04-12 02:12:06 +0100

    Add nix support

---
+++ lua/plugins/nix.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "nix",
+        })
+      end
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nixd = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+}
