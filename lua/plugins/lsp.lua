6e06488412e57d84885849232341cb8f1e57076d
Claudio Yanes <me@claudio4.com>
2024-04-19 15:35:15 +0100

    Allow new lsp kekybinds in insert mode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,8 +3,8 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "<F2>", vim.lsp.buf.rename, desc = "Rename" }
-      keys[#keys + 1] = { "<F3>", vim.lsp.buf.format, desc = "Format code (LSP)" }
+      keys[#keys + 1] = { "<F2>", vim.lsp.buf.rename, mode = { "n", "i" }, desc = "Rename" }
+      keys[#keys + 1] = { "<F3>", vim.lsp.buf.format, mode = { "n", "i" }, desc = "Format code (LSP)" }
     end,
   },
 }
