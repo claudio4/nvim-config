ecc5e68526c721e7f8913c37e730bc1f1b7ae4db
Claudio Yanes <me@claudio4.com>
2024-09-25 04:03:52 +0100

    Remove Go extension

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,7 +9,6 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.mini-move" },
