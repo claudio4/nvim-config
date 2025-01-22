4bbe579d36083d00b459055f450efc2314514b3f
Claudio Yanes <me@claudio4.com>
2025-01-22 14:56:01 +0000

    Add copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,11 +4,12 @@
 local map = vim.keymap.set
 
 -- Telescope/find
-map("n", "<leader>fp", "<cmd>Telescope commands<CR>", { desc = "Find Prompt" })
-map("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Find Keymap" })
+map("n", "<leader>fp", "<cmd>FzfLua commands<CR>", { desc = "Find Prompt" })
+map("n", "<leader>fk", "<cmd>FzfLua keymaps<CR>", { desc = "Find Keymap" })
 
 -- ui
 map("n", "<leader>ut", "<cmd>ToggleColorscheme<CR>", { desc = "Toggle between dark/light theme" })
 
 -- text edit
 map("i", "<C-BS>", "<C-w>", { noremap = true, desc = "delete whole word" })
+map("i", "<C-v>", "<C-R>+", { noremap = true, desc = "paste" })
